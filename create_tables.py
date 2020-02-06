40ec49133adbd0bfae37f81af92ad933837ebcd2
eunice <euniceboon@live.com.my>
2020-02-06 13:57:48 +0000

    formatted connections

--- create_tables.py
+++ create_tables.py
@@ -1,22 +1,36 @@
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
+import json
+# load configurations
+with open('config.json') as f:
+    config = f.read()
+config = json.loads(config)
 
 def create_database():
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn = psycopg2.connect("host={hostname} dbname={dbname} user={username} password={password}".format(
+        hostname= config.get("HOSTNAME"),
+        dbname= config.get("DB-NAME"),
+        username= config.get("USERNAME"),
+        password=config.get("PASSWORD")
+    ))
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     
     # create sparkify database with UTF8 encoding
-    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    cur.execute("DROP DATABASE IF EXISTS {dbname}".format(
+        dbname = config.get("DB-NAME")
+    ))
+    cur.execute("CREATE DATABASE {dbname} WITH ENCODING 'utf8' TEMPLATE template0".format(
+        dbname = config.get("DB-NAME")
+    ))
 
     # close connection to default database
     conn.close()    
     
     # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host={hostname} dbname={dbname} user={username} password={password}")
     cur = conn.cursor()
     
     return cur, conn
